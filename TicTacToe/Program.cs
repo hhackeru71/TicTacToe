1aef0feac98e9532e046f5fe2e2441a4d5c921ba
spaceshipwalls@gmail.com <spaceshipwalls@gmail.com>
2022-11-10 20:38:27 +0200

    .

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -4,11 +4,13 @@ namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
+        static char[] board = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
         static void Main(string[] args)
         {
             
 			try
 			{   int player = 1;
+                
                 string signPlayer2 = "";
                 Console.WriteLine("enter a name");
                 string namePlayer1 = Console.ReadLine();
@@ -45,7 +47,22 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 {
                   //  Console.Clear();//מחיקת המסך
                     Console.WriteLine($"player {namePlayer1} : {signPlayer1} and player 2 {namePlayer2} {signPlayer2}");
+                    if(player % 2 == 0)
+                    {
+                        Console.WriteLine($"name {namePlayer2} turn");
+                    }
+                    else
+                        Console.WriteLine($"name {namePlayer1} turn");
                     DrawBoard();
+                    int.TryParse(Console.ReadLine(), out int choice);
+                    while(choice < 1 || choice > 9)
+                    {
+                        Console.WriteLine("error number!enter a number between 1-9");
+                        int.TryParse(Console.ReadLine(), out choice);
+                    }
+                    choice -= 1;
+                    
+
                 }
                 while (true);
 
@@ -62,14 +79,15 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
         static void DrawBoard()
         {
+           
             Console.WriteLine("     |     |      ");
-            Console.WriteLine($"  {1}  |  {2}  |  {3}");
+            Console.WriteLine($"  {board[0]}  |  {board[1]}  |  {board[2]}");
             Console.WriteLine("_____|_____|_____ ");
             Console.WriteLine("     |     |      ");
-            Console.WriteLine($"  {4}  |  {5}  |  {6}");
+            Console.WriteLine($"  {board[3]}  |  {board[4]}  |  {board[5]}");
             Console.WriteLine("_____|_____|_____ ");
             Console.WriteLine("     |     |      ");
-            Console.WriteLine($"  {7}  |  {8}  |  {9}");
+            Console.WriteLine($"  {board[6]}  |  {board[7]}  |  {board[8]}");
             Console.WriteLine("     |     |      ");
         }
     }
